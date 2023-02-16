6e46754c717850c55f23e67805d73674a3a13cb7
bamigbola <faithbamigbola80@gmail.com>
2023-02-16 14:51:59 -0800

    c puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A line with puts function that prints a program
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
